0677c0d6c29f3d582ee3f5f9031e605e2e60fb80
bluebird <neverdream9106@gmail.com>
2022-10-18 10:34:45 -0400

    layout style is updated.

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -7,7 +7,7 @@ interface PropTypes {
 export default function Layout({ children }: PropTypes): JSX.Element {
     return (
         <div style={{display: 'flex'}}>
-            <div style={{ flex: '1 0 auto'}}>
+            <div style={{ flex: '1 0 auto', paddingTop: '40px', paddingBottom: '40px'}}>
                 {
                     children
                 }
